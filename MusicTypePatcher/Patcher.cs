1706b1a8bb3c61d17f031432b583c58450b0f84d
OddDrifter <legioxv@gmail.com>
2021-02-24 12:34:25 -0500

    Try to prevent unnecessary overrides.

--- MusicTypePatcher/Patcher.cs
+++ MusicTypePatcher/Patcher.cs
@@ -2,6 +2,7 @@
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
 using Noggog;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,23 +24,31 @@ namespace MusicTypePatcher
 
         private static void Apply(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            var enabledPlugins = state.LoadOrder.PriorityOrder.OnlyEnabled();
-            var query = enabledPlugins.SelectMany(_ => _.Mod?.MusicTypes)
-                .GroupBy(_ => _.FormKey, (k, v) => v)
-                .Where(_ => _.Count() > 2);
+            using var loadOrder = state.LoadOrder; 
+            var query = loadOrder.PriorityOrder.Reverse().OnlyEnabled()
+                .SelectMany(plugins => plugins.AsEnumerable().MusicType().WinningOverrides())
+                .GroupBy(record => record.FormKey);
 
             foreach(var value in query)
             {
-                var copy = state.PatchMod.MusicTypes.GetOrAddAsOverride(value.First());
+                bool shouldKeep = false;
+                var trackList = new ExtendedList<IFormLink<IMusicTrackGetter>>();
 
-                if (copy.Tracks == null)
-                    copy.Tracks = new ExtendedList<IFormLink<IMusicTrackGetter>>();
+                foreach (var musicType in value)
+                {
+                    shouldKeep = trackList.Except(musicType.Tracks).Any();
+                    foreach (var tracks in musicType.Tracks.GroupBy(_ => _.FormKey))
+                    {
+                        trackList.AddRange(tracks.Skip(trackList.Count(_ => _.FormKey == tracks.Key)));
+                    }
+                }
 
-                foreach (var trackList in value.Select(_ => _.Tracks))
-                {         
-                    copy.Tracks.AddRange(
-                        trackList.Where(track =>
-                            copy.Tracks.Count(_ => _.FormKey.Equals(track.FormKey)) < trackList.Count(_ => _.FormKey.Equals(track.FormKey))));
+                if (shouldKeep)
+                {
+                    var copy = new MusicType(value.Key, SkyrimRelease.SkyrimSE) { Tracks = trackList };
+                    copy.DeepCopyIn(value.First(), new MusicType.TranslationMask(defaultOn: true) { Tracks = false });
+                    state.PatchMod.MusicTypes.GetOrAddAsOverride(copy);
+                    Console.WriteLine("Copied {0} tracks to {1}", copy.Tracks.Count() - value.First().Tracks.Count(), copy.EditorID);
                 }
             }
         }
